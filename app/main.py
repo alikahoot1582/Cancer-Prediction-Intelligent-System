7c444277f79a05a31aebcbd84ea57fd46563e4b5
alikahoot1582 <alikahoot1582@gmail.com>
2026-04-16 12:53:10 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -139,7 +139,7 @@ def generate_ai_analysis(input_dict, prediction, probs, mode):
 # -------------------- MAIN --------------------
 def main():
     st.set_page_config(
-        page_title=" 🩺 Breast Cancer Risk Assistant"
+        page_title=" 🩺 Breast Cancer Risk Assistant",
         page_icon= "🩺" ,
         layout="wide"
     )
